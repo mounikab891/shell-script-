6d77e5eeb05e5dc36085c01dd65c85ef520897d6
Mounika Bethu <113983011+mounikab891@users.noreply.github.com>
2023-06-06 17:45:03 +0530

    Update targetgrouptags.sh

--- targetgrouptags.sh
+++ targetgrouptags.sh
@@ -5,12 +5,12 @@ lbarnlist=$(aws elbv2 describe-load-balancers --profile $profile --query 'LoadBa
 for LARN in $lbarnlist
 do
 #get the list of particular loadbalancer attached target groups 
-tgarnlist=$(aws elbv2 describe-target-groups --profile $profile --load-balancer-arn arn:aws:elasticloadbalancing:ap-south-1:247653494814:loadbalancer/net/stg-apm-lb/c8bceedd0dac42c5 --query 'TargetGroups[*].TargetGroupArn' | tr -d "[]\","| sed '/^[[:space:]]*$/d')
+tgarnlist=$(aws elbv2 describe-target-groups --profile $profile --load-balancer-arn arn:aws:elasticloadbalancing:XXXXXXXX --query 'TargetGroups[*].TargetGroupArn' | tr -d "[]\","| sed '/^[[:space:]]*$/d')
 
 for i in $tgarnlist
 do 
 #adding the tags to all targetgroups
-aws elbv2 add-tags --profile $profile --resource-arns $i --tags "Key=Environment,Value=Production"
+aws elbv2 add-tags --profile $profile --resource-arns $i --tags "Key=Environment,Value=XXXXX"
 
     done
     done
